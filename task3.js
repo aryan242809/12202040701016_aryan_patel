92c4367f68a9d58fa0cb1eee12142520c8531d35
000 <000@aditdata.com>
2025-08-25 15:31:35 +0530

    updated

---
+++ task3.js
@@ -0,0 +1,15 @@
+let meetings=
+[
+    {id: 1, start: 1, end: 4},
+    {id: 2, start: 3, end: 5},
+    {id: 3, start: 0, end: 6},
+    {id: 4, start: 5, end: 7},
+    {id: 5, start: 8, end: 9},
+    {id: 6, start: 5, end: 9}
+  ]
+  
+  meetings.forEach((e) =>
+{
+    let id = e.id
+    let startnum = e.start
+})
